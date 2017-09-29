a8cbe722b90982c6a16cc9d00179035ba6dd0a70
tinyherocarrot <andrw.huang@gmail.com>
2017-09-29 13:25:58 -0500

    added functionality for search and clear btns

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,39 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "083040deb76744ca8c92a2ec79536c0d",
-  'q': "trump",
-  'begin_date': "20170101"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var searchInput;
+var limit;
+var startYr;
+var endYr;
+
+
+$("#searchbtn").on("click", function() {
+	event.preventDefault();
+	
+	searchInput = $("#search").val().trim();
+	limit = $("#records").val().trim();
+	startYr = $("#start").val().trim()
+	endYr = $("#end").val().trim();
+
+	console.log(searchInput);
 });
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+
+$("#clearbtn").on("click", function() {
+	$("#display").empty();
 });
+
+// Build query URL here
+queryURL +
+// url += '?' + $.param({
+//   'api-key': "083040deb76744ca8c92a2ec79536c0d",
+//   'q': "trump",
+//   'begin_date': "20170101"
+// });
+
+
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
